f3d0a17001235f704a653e703865e029931c53f0
Steve Wigfield <sjwigfield@gmail.com>
2017-03-25 17:32:05 +0000

    Booleans

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonPress(_ sender: Any) {
-        theLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        let addition = true
+        
+        if addition {
+            theLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer is: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
     }
     
     override func viewDidLoad() {
@@ -24,12 +31,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
