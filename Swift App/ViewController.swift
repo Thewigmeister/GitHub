953ec1e826f5aab554f9da0f3ed33270f4e8f6d3
Steve Wigfield <sjwigfield@gmail.com>
2017-03-25 16:51:35 +0000

    Text boxes for addition later

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        print(text1.text!)
+        print(text2.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
